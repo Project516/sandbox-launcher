7102771479092ee4a8cefcc3ce36993b2cc97fdc
project516 <138796702+Project516@users.noreply.github.com>
2026-07-12 12:01:56 -0500

    zip jpackage

--- jpackage.sh
+++ jpackage.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+rm -rf sandbox-launcher
+
 jpackage --input app/build/libs/ \
   --name sandbox-launcher \
   --main-jar app-all.jar \
   --main-class dev.project516.sandbox.Main \
   --type app-image \
   --java-options '--enable-preview'
+
+zip -r sandbox-launcher.zip sandbox-launcher
+
+rm -rf sandbox-launcher
